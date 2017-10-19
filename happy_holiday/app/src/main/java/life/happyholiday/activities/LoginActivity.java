99713ab51886069a570666523cad018bcb39ca39
liuyang <a0077978@gmail.com>
2017-10-19 19:24:26 +0800

    Add google sign in support
    Get token from google

--- happy_holiday/app/src/main/java/life/happyholiday/activities/LoginActivity.java
+++ happy_holiday/app/src/main/java/life/happyholiday/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package life.happyholiday.activities;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -9,13 +10,24 @@ import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.realm.SyncCredentials;
+import io.realm.SyncUser;
 import life.happyholiday.R;
+import life.happyholiday.utils.auth.GoogleAuth;
+import timber.log.Timber;
 
 public class LoginActivity extends BaseActivity {
+    private static final int RC_SIGN_IN = 9001;
+
     @BindView(R.id.logo)
     View logo;
 
@@ -28,16 +40,39 @@ public class LoginActivity extends BaseActivity {
     @BindView(R.id.btn_login)
     Button btnLogin;
 
+    private GoogleAuth googleAuth;
+    private ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        // Setup Google Authentication
+        googleAuth = new GoogleAuth((SignInButton) findViewById(R.id.sign_in_button), this) {
+            @Override
+            public void onRegistrationComplete(GoogleSignInResult result) {
+//                UserManager.setAuthMode(UserManager.AUTH_MODE.GOOGLE);
+                GoogleSignInAccount acct = result.getSignInAccount();
+                Toast.makeText(LoginActivity.this, acct.getDisplayName() + " " + acct.getEmail() + " " + acct.getIdToken(), Toast.LENGTH_LONG).show();
+//                SyncCredentials credentials = SyncCredentials.google(acct.getIdToken());
+//                SyncUser.loginAsync(credentials, AUTH_URL, SignInActivity.this);
+            }
+
+            @Override
+            public void onError(String s) {
+                super.onError(s);
+            }
+        };
+
+
+        // Prepare for animations
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int height = displayMetrics.heightPixels;
 
+        // Animation
         ObjectAnimator moveUpLogo = ObjectAnimator.ofFloat(logo, "translationY", height * -0.14f);
         ObjectAnimator moveDownLoginLayout = ObjectAnimator.ofFloat(layoutLogin, "translationY", height * 0.08f);
         ObjectAnimator fadeInLoginLayout = ObjectAnimator.ofFloat(layoutLogin, "alpha", 0, 1);
@@ -57,7 +92,31 @@ public class LoginActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
 
+        hideProgressDialog();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
     }
 
     @OnClick(R.id.logo)
@@ -70,5 +129,11 @@ public class LoginActivity extends BaseActivity {
         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        googleAuth.onActivityResult(requestCode, resultCode, data);
     }
 }
